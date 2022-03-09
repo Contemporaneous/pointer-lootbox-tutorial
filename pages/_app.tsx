ddd851706cc66bfd50820381b890827d402fec49
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-03-10 06:39:20 +1000

    Update _app wiyth thirdweb support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,29 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Head from "next/head";
+import { ThirdwebProvider } from "@3rdweb/react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  const supportedChainIds = [80001];
+
+  const connectors = {
+    injected: {},
+  };
+
   return (
     <>
       <Head>
         <title>{pageProps.title}</title>
       </Head>
-      <Layout title={pageProps.title}>
-        <Component {...pageProps} />
-      </Layout>
+      <ThirdwebProvider
+        connectors={connectors}
+        supportedChainIds={supportedChainIds}
+      >
+        <Layout title={pageProps.title}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThirdwebProvider>
     </>
   );
 }
